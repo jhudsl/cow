7b31a699a7ebdfe23dffc4acb7f18e0a0a85935c
Candace Savonen <cansav09@gmail.com>
2021-12-02 09:41:30 -0500

    test GHA

--- update-googlesheet.R
+++ update-googlesheet.R
@@ -2,19 +2,25 @@
 # C. Savonen 2021
 
 if (!(installed.packages() %in% "remotes")){
-  install.packages("remotes")
+  install.packages("devtools")
 }
+
+# We will load the latest gitHelpeR package root
+root_dir <- rprojroot::find_root(rprojroot::has_file("gitHelpeR.Rproj"))
 
 if (!(installed.packages() %in% "gitHelpeR")){
-  remotes::install_github("jhudsl/gitHelpeR")
+  devtools::load_all(root_dir)
 }
 
+# Run onjhudsl org and retrieve keywords and learning objectives
 chapter_df <-
   gitHelpeR::retrieve_org_chapters(org_name = "jhudsl",
                                    output_file = "jhudsl_chapter_info.tsv",
                                    retrieve_learning_obj = TRUE,
-                                   retrieve_keywords = TRUE)
+                                   retrieve_keywords = TRUE,
+                                   verbose = FALSE)
 
+# Save to googlseheet
 googlesheets4::sheet_write(data = chapter_df,
   googledrive::as_id("https://docs.google.com/spreadsheets/d/14KYZA2K3J78mHVCiWV6-vkY6it37Ndxnow1Uu7nMa80/edit#gid=0"),
   sheet = 1)
