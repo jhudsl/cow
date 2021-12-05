260bfdfe23ee81fb6df80e05e773512277c7087f
Candace Savonen <cansav09@gmail.com>
2021-12-04 21:49:39 -0500

    This auth should work

--- update-googlesheet.R
+++ update-googlesheet.R
@@ -1,66 +1,28 @@
 # Update the jhu course library googlesheet
 # C. Savonen 2021
 
-############################## Set up Functions ################################
-authorize_from_secret <- function(access_token, refresh_token) {
-  client_id <- getOption("slides.client.id")
-  client_secret <- getOption("slides.client.secret")
-
-  credentials <- list(
-    access_token = access_token,
-    expires_in = 3599L,
-    refresh_token = refresh_token,
-    scope = "https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/userinfo.email openid",
-    token_type = "Bearer"
-  )
-
-  app <- httr::oauth_app(
-    appname = "googleslides",
-    key = client_id,
-    secret = client_secret
-  )
-  endpoint <- httr::oauth_endpoints("google")
-
-  token <- httr::oauth2.0_token(
-    endpoint = endpoint, app = app,
-    scope = c(
-      "https://www.googleapis.com/auth/presentations",
-      "https://www.googleapis.com/auth/drive.readonly"
-    ),
-    credentials = credentials
-  )
-
-  return(token)
-}
-
 library(optparse)
+
+# Set up auth
+options(
+  gargle_oauth_cache = ".secrets",
+  gargle_oauth_email = "cansav09@gmail.com"
+)
+
+googlesheets4::gs4_auth(cache = ".secrets", email = "cansav09@gmail.com")
 
 ################################ Set up options ################################
 # Set up optparse options.
 option_list <- list(
   make_option(
-    opt_str = c("-r", "--refresh_token"), type = "character",
-    default = NULL, help = "Can be obtained from: auth <- googlesheets4::gs4_auth_configure() auth$cred$credentials",
-    metavar = "character"
-  ),
-  make_option(
-    opt_str = c("-a", "--access_token"), type = "character",
-    default = NULL, help = "Can be obtained from: auth <- googlesheets4::gs4_auth_configure() auth$cred$credentials",
+    opt_str = c("-t", "--token_rds"), type = "character",
+    default = NULL, help = "path to google sheets token saved to rds file",
     metavar = "character"
   )
 )
 
 # Parse options
 opt <- parse_args(OptionParser(option_list = option_list))
-
-# Set up token
-token <- authorize_from_secret(
-  opt$access_token,
-  opt$refresh_token
-)
-
-# set up auth
-googlesheets4::gs4_auth(token = token)
 
 # We will load the latest gitHelpeR package root
 root_dir <- rprojroot::find_root(rprojroot::has_file("gitHelpeR.Rproj"))
@@ -72,7 +34,7 @@ devtools::load_all(root_dir)
 chapter_df <-
   gitHelpeR::retrieve_org_chapters(
     org_name = "jhudsl",
-    git_pat = readLines("git_token.txt"),
+    git_pat = readLines("git_token.txt")[1],
     output_file = "jhudsl_chapter_info.tsv",
     retrieve_learning_obj = TRUE,
     retrieve_keywords = TRUE,
